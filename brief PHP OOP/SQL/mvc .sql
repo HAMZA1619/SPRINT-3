b5e327ac6ca736f1c61d201d773bfa0d226b3932
hamza elglaoui <chyoin188@gmail.com>
2021-05-22 00:56:35 +0000

    changes in database sql

--- brief PHP OOP/SQL/mvc .sql
+++ brief PHP OOP/SQL/mvc .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 21 mai 2021 à 17:41
+-- Généré le : sam. 22 mai 2021 à 02:53
 -- Version du serveur :  10.4.17-MariaDB
 -- Version de PHP : 8.0.2
 
@@ -38,6 +38,14 @@ CREATE TABLE `apprenant` (
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `apprenant`
+--
+
+INSERT INTO `apprenant` (`id`, `nom`, `prenom`, `genre`, `age`, `id_class`, `id_user`, `date`) VALUES
+(31, 'hamza', 'elglaoui', 'male', 20, 'grad 1', 38, '2021-05-21 18:21:09'),
+(32, 'hind', 'toto', 'female', 22, 'grad 2', 38, '2021-05-21 18:22:14');
+
 -- --------------------------------------------------------
 
 --
@@ -46,12 +54,21 @@ CREATE TABLE `apprenant` (
 
 CREATE TABLE `class` (
   `id` int(11) NOT NULL,
-  `nom` varchar(11) NOT NULL,
-  `specialite` varchar(11) NOT NULL,
+  `nom` varchar(60) NOT NULL,
+  `specialite` varchar(60) NOT NULL,
   `desception` text NOT NULL,
   `id_user` int(11) NOT NULL,
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `class`
+--
+
+INSERT INTO `class` (`id`, `nom`, `specialite`, `desception`, `id_user`, `date`) VALUES
+(22, 'grad 1', 'somthing', 'i don\'t know', 38, '2021-05-21 18:20:36'),
+(23, 'grad 2', 'somthing', 'somthing', 38, '2021-05-21 18:21:44'),
+(24, 'grad 3', 'somthing', 'somthing', 38, '2021-05-21 18:23:07');
 
 -- --------------------------------------------------------
 
@@ -61,11 +78,18 @@ CREATE TABLE `class` (
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `email` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `date`) VALUES
+(38, 'hamza', 'chyoin188@gmail.com', '$2y$10$zcdpnchr1LQH9rNS1Yzd2.ihcE0vnxnuBHxVKcR57vEnxptBWxaZG', '2021-05-21 18:19:40');
 
 --
 -- Index pour les tables déchargées
@@ -75,7 +99,8 @@ CREATE TABLE `user` (
 -- Index pour la table `apprenant`
 --
 ALTER TABLE `apprenant`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Index pour la table `class`
@@ -98,23 +123,29 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `apprenant`
 --
 ALTER TABLE `apprenant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT pour la table `class`
 --
 ALTER TABLE `class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `apprenant`
+--
+ALTER TABLE `apprenant`
+  ADD CONSTRAINT `apprenant_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `class`
