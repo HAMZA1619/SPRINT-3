285b6ee411c61b6e01ec188c690d191e7deca8f7
hamza elglaoui <chyoin188@gmail.com>
2021-05-24 14:41:35 +0000

    forgot password send mail

--- brief PHP OOP/code source/app/core/mail.php
+++ brief PHP OOP/code source/app/core/mail.php
@@ -1,29 +1,29 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+// //Import PHPMailer classes into the global namespace
+// //These must be at the top of your script, not inside a function
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
 
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+// //Load Composer's autoloader
+// require 'vendor/autoload.php';
 
-//Instantiation and passing `true` enables exceptions
-$mail = new PHPMailer();
+// //Instantiation and passing `true` enables exceptions
+// $mail = new PHPMailer();
 
-   //Server settings
-   // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-   $mail->isSMTP();                                            //Send using SMTP
-   $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-   $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-   $mail->Username   = 'hamzaelg32@gmail.com';                     //SMTP username
-   $mail->Password   = 'hamza0651018901';                               //SMTP password
-   $mail->SMTPSecure = 'ssl';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-   $mail->Port       = 465; 
+//    //Server settings
+//    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+//    $mail->isSMTP();                                            //Send using SMTP
+//    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+//    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+//    $mail->Username   = 'hamzaelg32@gmail.com';                     //SMTP username
+//    $mail->Password   = 'hamza0651018901';                               //SMTP password
+//    $mail->SMTPSecure = 'ssl';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+//    $mail->Port       = 465; 
    
    
-    //Content
-    $mail->isHTML(true);    
+//     //Content
+//     $mail->isHTML(true);    
 
 
 
